1cb7809de95bbd2aab2c115ea8b8d055aebc91b9
kyleqihua <kyleqihuahuang@gmail.com>
2025-11-30 15:49:30 +0800

    Add .gitignore and CodeBreaker project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by kyle qihua  on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
