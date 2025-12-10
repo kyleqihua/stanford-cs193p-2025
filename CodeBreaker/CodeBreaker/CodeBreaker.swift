094cb6d8624b05e874db588185639267a81e7a3b
kyleqihua <kyleqihuahuang@gmail.com>
2025-12-10 14:02:52 +0800

    add Code struct with Kind enum

--- CodeBreaker/CodeBreaker/CodeBreaker.swift
+++ CodeBreaker/CodeBreaker/CodeBreaker.swift
@@ -13,3 +13,16 @@ struct CodeBreaker {
     var attempts: [Code]
     let pegChoices: [Peg]
 }
+
+struct Code {
+    var pegs: [Peg]
+    var kind: Kind
+
+    enum Kind {
+        case master
+        case guess
+        case attempt
+    }
+}
+
+typealias Peg = Color
